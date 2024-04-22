e63a1e8e16d1fe432af3bc3ecbafc4096ac9ff36
waldohidalgo <whidalgohp@gmail.com>
2024-04-22 18:53:40 -0400

    change order folder and files

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
-import app from "../index.js";
+import express from "express";
+import router from "./router/router.js";
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`El servidor está inicializado en el puerto ${PORT}`);
+});
+
+app.use("/", router);
 
 export default app;
